5e60e28bdb31dfe2e446c4ae837de33bac3bfd30
a-perkin <a-perkin@WIN-RO4I3HMKFU6>
2020-10-25 20:27:20 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AppService } from "./app.service";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+  constructor(private appService: AppService) {}
+
   title = 'ng-app';
+  orders: string = '';
+
+  getAllOrders() {
+    this.appService.getOrders().pipe().subscribe((response: any) => {
+      if (response && response.message)
+      this.orders = response.message;
+    });
+  }
 }
